a3f64652d24370707777fef4cb9d5fdbb3b9ff4d
Rajeev Majhi <rajeevkumarmajhi@gmail.com>
2024-06-23 22:53:06 +0545

    working sms integration

---
+++ android/app/src/main/kotlin/com/example/telephonysms/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.telephonysms
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:telephony/telephony.dart';
+
+onBackgroundMessage(SmsMessage message) {
+  debugPrint("onBackgroundMessage called");
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  String _message = "";
+  final telephony = Telephony.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  onMessage(SmsMessage message) async {
+    setState(() {
+      _message = message.body ?? "Error reading message body.";
+    });
+  }
+
+  onSendStatus(SendStatus status) {
+    setState(() {
+      _message = status == SendStatus.SENT ? "sent" : "delivered";
+    });
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+
+    final bool? result = await telephony.requestPhoneAndSmsPermissions;
+
+    if (result != null && result) {
+      telephony.listenIncomingSms(
+          onNewMessage: onMessage, onBackgroundMessage: onBackgroundMessage);
+    }
+
+    if (!mounted) return;
+  }
+
+  // Function to send SMS
+  void sendSms() async {
+    try {
+      await telephony.sendSms(
+        to: "9800930444",
+        message: "This is test",
+        statusListener: onSendStatus,
+      );
+    } catch (e) {
+      setState(() {
+        _message = "Failed to send SMS: $e";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(child: Text("Latest received SMS: $_message")),
+            const SizedBox(
+                height: 20), // Add some space between the text and button
+            ElevatedButton(
+              onPressed: sendSms,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue, // Button color
+              ),
+              child: const Text(
+                'Send SMS',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

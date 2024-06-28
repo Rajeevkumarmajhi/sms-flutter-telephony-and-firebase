62ba7f0a0d3c63f552d9d4d36b74b7f9b53c9b9d
Rajeev Majhi <rajeevkumarmajhi@gmail.com>
2024-06-29 00:03:25 +0545

    auto sending sms using firebase and laravel api

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:convert'; // Add this import
 import 'package:telephony/telephony.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:http/http.dart' as http;
 
-onBackgroundMessage(SmsMessage message) {
-  debugPrint("onBackgroundMessage called");
+// Add this function for background message handling
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // print("Handling a background message: ${message.messageId}");
 }
 
-void main() {
+onBackgroundMessage(SmsMessage message) {
+  // debugPrint("onBackgroundMessage called");
+}
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  const InitializationSettings initializationSettings =
+      InitializationSettings(android: initializationSettingsAndroid);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
   runApp(const MyApp());
 }
 
@@ -19,12 +42,86 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
   String _message = "";
+  String _fcmToken = ""; // Added to store FCM token
   final telephony = Telephony.instance;
+  late final FirebaseMessaging _firebaseMessaging;
 
   @override
   void initState() {
     super.initState();
+    _firebaseMessaging =
+        FirebaseMessaging.instance; // Initialize Firebase Messaging
     initPlatformState();
+    _initializeFirebaseMessaging();
+  }
+
+  void _initializeFirebaseMessaging() {
+    _firebaseMessaging.getToken().then((String? token) {
+      setState(() {
+        _fcmToken = token ?? "";
+      });
+      _sendTokenToServer(token ?? ""); // Save token to server
+    });
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      // print('Got a message whilst in the foreground!');
+      // print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        // print('Message also contained a notification: ${message.notification}');
+        _showNotification(
+            message.notification?.title, message.notification?.body);
+        if (message.data.containsKey('message')) {
+          sendSms(message.data['message']);
+        }
+      }
+    });
+  }
+
+  void _showNotification(String? title, String? body) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails('your_channel_id', 'your_channel_name',
+            channelDescription: 'your_channel_description',
+            importance: Importance.max,
+            priority: Priority.high,
+            ticker: 'ticker');
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      title,
+      body,
+      platformChannelSpecifics,
+      payload: 'item x',
+    );
+  }
+
+  void _sendTokenToServer(String token) async {
+    // Replace with your server URL
+    const String serverUrl = 'http://sipbazar.com.np/api/fcm-token';
+
+    // Set up the request
+    final response = await http.post(
+      Uri.parse(serverUrl),
+      headers: <String, String>{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'secret_key': '!sipbazar@2024!',
+        'fcm_token': token,
+      }),
+    );
+
+    // Handle the response
+    if (response.statusCode == 200) {
+      // If the server returns a 200 OK response, parse the JSON
+      // print('Token saved successfully');
+    } else {
+      // If the server did not return a 200 OK response,
+      // throw an exception.
+      // print('Failed to save token: ${response.statusCode}');
+    }
   }
 
   onMessage(SmsMessage message) async {
@@ -57,11 +154,11 @@ class MyAppState extends State<MyApp> {
   }
 
   // Function to send SMS
-  void sendSms() async {
+  void sendSms(String message) async {
     try {
       await telephony.sendSms(
         to: "9800930444",
-        message: "This is test",
+        message: message,
         statusListener: onSendStatus,
       );
     } catch (e) {
@@ -82,10 +179,13 @@ class MyAppState extends State<MyApp> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Center(child: Text("Latest received SMS: $_message")),
+            Center(
+              child: Text("FCM token: $_fcmToken"),
+            ),
             const SizedBox(
                 height: 20), // Add some space between the text and button
             ElevatedButton(
-              onPressed: sendSms,
+              onPressed: () => sendSms("This is test"),
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.blue, // Button color
               ),
